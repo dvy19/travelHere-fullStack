dfe285a991f9262eb4445b93a4b377951d52d39c
dvy19 <divyadixit.311204@gmail.com>
2026-07-11 21:00:26 +0530

    header ui made

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+   
+
+        <>
+            <div className="main-header">
+                <div className="header-heading">
+                    Explore the Whole World and Enjoy the World's Beauty
+                </div>
+            </div>
+
+            <div className="header-bar">
+
+            </div>
+        </>
+    
+  )
+}
+
+export default Header
